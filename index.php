4c5dbfb3ccbf6c7d227a7537d41fa6b86980e7cd
Andre Luan <andreluan176@gmail.com>
2021-03-04 14:12:15 -0300

    Caminhos

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
 
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/core/core.php";
+require_once "app/core/core.php";
 
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/HomeController.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/ClienteController.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/ErroController.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/LoginController.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/PedidoController.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/controller/ProdutoController.php";
+require_once "app/controller/HomeController.php";
+require_once "app/controller/ClienteController.php";
+require_once "app/controller/ErroController.php";
+require_once "app/controller/LoginController.php";
+require_once "app/controller/PedidoController.php";
+require_once "app/controller/ProdutoController.php";
 
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/model/Cliente.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/model/Funcionario.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/model/Pedidos.php";
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/app/model/Produto.php";
+require_once "app/model/Cliente.php";
+require_once "app/model/Funcionario.php";
+require_once "app/model/Pedidos.php";
+require_once "app/model/Produto.php";
 
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/lib/database/Connection.php";
+require_once "lib/database/Connection.php";
 
-require_once "/opt/lampp/htdocs/Projeto_PHP_OO/vendor/autoload.php";
+require_once "vendor/autoload.php";
 
 ob_start();
     $core = new Core();
